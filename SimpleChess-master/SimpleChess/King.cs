7597bbb7a45ea98e910416850f24922835278658
Erik Isaksen <72092585+Erikisaksen93@users.noreply.github.com>
2021-03-15 13:27:01 +0100

    Initial commit

---
+++ SimpleChess-master/SimpleChess/King.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+    class King : Piece
+    {
+        public King() : base("KNG") { }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var diffCol = fromPosition[0] - toPosition[0];
+            var diffRow = fromPosition[1] - toPosition[1];
+            if ((Math.Abs(diffRow) == Math.Abs(diffCol)) || (Math.Abs(diffRow) + 1 == Math.Abs(diffCol)) ||
+                Math.Abs(diffRow) == Math.Abs(diffCol) + 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+            
+        }
+    }
+}
